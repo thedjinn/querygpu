31c4f793669b2455f2388843352f6ace43db79d0
Emil Loer <emil@koffietijd.net>
2023-01-11 22:23:58 +0100

    Add some more documentation

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,20 @@ fn main() {
     unsafe {
         let mut iterator: io::io_iterator_t = io::IO_OBJECT_NULL;
 
+        // Create a service name matching dictionary for the graphics control service. This creates
+        // a new CFDictionary that is later consumed by IOServiceGetMatchingServices, thus not
+        // requiring a release.
         let service = io::IOServiceMatching(kDriverClassName.as_ptr() as *const std::ffi::c_char);
 
+        // Create a services iterator that finds the graphics control service.
         let result =
             io::IOServiceGetMatchingServices(io::kIOMasterPortDefault, service, &mut iterator);
         if result != mach::kern_return::KERN_SUCCESS {
             panic!("IOServiceGetMatchingServices returned non-zero result");
         }
 
+        // Since there is only one graphics control service, the next item will always be the
+        // service that should be used. The iterator is released afterwards.
         let service = io::IOIteratorNext(iterator);
         let result = io::IOObjectRelease(iterator);
         if result != mach::kern_return::KERN_SUCCESS {
@@ -37,6 +43,7 @@ fn main() {
             panic!("No matching driver found");
         }
 
+        // Open a connection to the service.
         let result = io::IOServiceOpen(
             service,
             mach::traps::mach_task_self() as mach::mach_types::task_port_t,
@@ -47,6 +54,7 @@ fn main() {
             panic!("IOServiceOpen returned non-zero result");
         }
 
+        // Call the open method on the connection to finalize the connection.
         let result = io::IOConnectCallScalarMethod(
             connection,
             kOpen,
@@ -59,6 +67,8 @@ fn main() {
             panic!("IOConnectCallScalarMethod(kOpen) returned non-zero result");
         }
 
+        // Invoke the GetMuxState method on the service with the muxGraphicsCard subcommand to get
+        // the currently active GPU.
         let input = [1u64, muxGraphicsCard];
         let mut output_count = 1u32;
 
@@ -75,6 +85,7 @@ fn main() {
         }
     }
 
+    // Print the result message
     match output {
         0 => println!("Currently using the discrete GPU"),
         1 => println!("Currently using the integrated GPU"),
@@ -82,7 +93,7 @@ fn main() {
     }
 
     unsafe {
-        // Close switcher
+        // Finalize the connection
         let result = io::IOConnectCallScalarMethod(
             connection,
             kClose,
@@ -95,6 +106,7 @@ fn main() {
             panic!("IOConnectCallScalarMethod(kClose) returned non-zero result");
         }
 
+        // Close the service
         let result = io::IOServiceClose(connection);
         if result != mach::kern_return::KERN_SUCCESS {
             panic!("IOServiceClose returned non-zero result");
